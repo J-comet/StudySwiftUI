9b0d8fffcf9275923a3361e20875d33d71111d6c
hyeseong <hyeseong7848@gmail.com>
2023-11-13 09:43:59 +0900

    Initial Commit

---
+++ StudySwiftUI/StudySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUIApp.swift
+//  StudySwiftUI
+//
+//  Created by 장혜성 on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
